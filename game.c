2bbe74e287499d75e1719de4ccab619d01d4a30a
Karan Ram <Karanram730005@gmail.com>
2025-12-10 21:00:31 +0530

    adding game

--- game.c
+++ game.c
@@ -1,26 +1,7 @@
 #include <stdio.h>
 
-void final(int num)
-{
-    printf("1: All that glitters is not gold, Game Over!\n2: All your efforts were for nothing, Game Over!\n3: Congratulations!! You won the treasure.");
-    scanf("%d", &num);
-    if (num == 1)
-    {
-        printf("All that glitters is not gold, Game Over!");
-    }
-    else if (num == 2)
-    {
-        printf("All your efforts were for nothing, Game Over!");
-    }
-    else if (num == 3)
-    {
-        printf("Congratulations!! You won the treasure.");
-    }
-}
-
 int main()
 {
-
     int first, left, left_two, middle, right, num;
     scanf("%d", &first);
 
@@ -28,94 +9,77 @@ int main()
     {
         printf("Player chooses the Left path.\n");
         scanf("%d", &left);
+
         if (left == 1)
         {
-            printf("Poor choice, Game Over!");
+            printf("Poor choice, Game Over!\n");
         }
         else if (left == 2)
         {
             printf("Player found a bridge.\n");
             scanf("%d", &left_two);
+
             if (left_two == 1)
             {
                 printf("Player crosses the bridge safely.\n");
                 scanf("%d", &num);
+
                 if (num == 1)
-                {
-                    printf("All that glitters is not gold, Game Over!");
-                }
+                    printf("All that glitters is not gold, Game Over!\n");
                 else if (num == 2)
-                {
-                    printf("All your efforts were for nothing, Game Over!");
-                }
+                    printf("All your efforts were for nothing, Game Over!\n");
                 else if (num == 3)
-                {
-                    printf("Congratulations!! You won the treasure.");
-                }
+                    printf("Congratulations!! You won the treasure.\n");
             }
             else if (left_two == 2)
             {
-                printf("Poor luck, Game Over!");
+                printf("Poor luck, Game Over!\n");
             }
         }
     }
     else if (first == 2)
     {
-        printf("Player chooses the Middle path\n");
-        printf("I am a three-digit number. My second digit is four times bigger than the third digit, my first digit is three less than the second digit, and the sum of my digits is 15. What number am I?\n");
+        printf("Player chooses the Middle path.\n");
         scanf("%d", &middle);
+
         if (middle == 582)
         {
             printf("Player solved the puzzle.\n");
             scanf("%d", &num);
+
             if (num == 1)
-            {
-                printf("All that glitters is not gold, Game Over!");
-            }
+                printf("All that glitters is not gold, Game Over!\n");
             else if (num == 2)
-            {
-                printf("All your efforts were for nothing, Game Over!");
-            }
+                printf("All your efforts were for nothing, Game Over!\n");
             else if (num == 3)
-            {
-                printf("Congratulations!! You won the treasure.");
-            }
+                printf("Congratulations!! You won the treasure.\n");
         }
         else
         {
-            printf("Foolish player, Game Over!");
+            printf("Foolish player, Game Over!\n");
         }
     }
     else if (first == 3)
     {
-        printf("Player chooses the Right path\n");
-        printf("What number comes next in the sequence: 2, 6, 12, 20, __?\n");
+        printf("Player chooses the Right path.\n");
         scanf("%d", &right);
+
         if (right == 30)
         {
             printf("Player solved the puzzle.\n");
             scanf("%d", &num);
+
             if (num == 1)
-            {
-                printf("All that glitters is not gold, Game Over!");
-            }
+                printf("All that glitters is not gold, Game Over!\n");
             else if (num == 2)
-            {
-                printf("All your efforts were for nothing, Game Over!");
-            }
+                printf("All your efforts were for nothing, Game Over!\n");
             else if (num == 3)
-            {
-                printf("Congratulations!! You won the treasure.");
-            }
+                printf("Congratulations!! You won the treasure.\n");
         }
         else
         {
-            printf("Foolish player, Game Over!");
+            printf("Foolish player, Game Over!\n");
         }
-    }
-    else
-    {
-        printf("Invalid Choice.");
     }
 
     return 0;
