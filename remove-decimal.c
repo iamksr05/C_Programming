07f739fea18cf8423fe326453a03589296d0d765
Karan Ram <Karanram73005@gmail.com>
2026-02-04 14:34:08 +0530

    adding file

---
+++ remove-decimal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int value;
+    scanf("%d", &value);
+    printf("Rounded Value = %d", value/1);
+    return 0;
+}
