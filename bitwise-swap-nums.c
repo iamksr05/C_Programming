dd44eb2369a1101d2bf1fc73a5e096b58b3fd2a2
Karan Ram <Karanram73005@gmail.com>
2026-02-05 14:22:27 +0530

    adding file

---
+++ bitwise-swap-nums.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int A, B;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    A = A ^ B;
+    B = A ^ B;
+    A = A ^ B;
+
+    printf("%d\n", A);
+    printf("%d\n", B);
+
+    return 0;
+}
