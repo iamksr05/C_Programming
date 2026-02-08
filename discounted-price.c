f6fd1faf3086b45b9b7ae0f6afc02291cc9d0c3c
Karan Ram <Karanram73005@gmail.com>
2026-02-08 23:17:53 +0530

    added file

---
+++ discounted-price.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+float discounted(float price, float percent) {
+    float discountAmount = ( price * percent ) / 100;
+    float discountedPrice = price - discountAmount;
+    return discountedPrice;
+}
+int main() {
+
+    float price, percent;
+    scanf("%f %f", &price, &percent);
+    printf("The final price is: %f", discounted(price, percent));
+       
+    return 0;
+}
