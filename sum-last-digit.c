40e9356520e4e651802b673697544431c1ea969c
Karan Ram <Karanram73005@gmail.com>
2026-02-09 10:25:05 +0530

    added file

---
+++ sum-last-digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int lastSum(int a, int b) {
+
+    return (a % 10) + (b % 10);
+}
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("The sum of last digits is: %d", lastSum(a, b));
+    return 0;
+}
