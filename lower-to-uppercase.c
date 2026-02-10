c9d8389df9785ac93b5f17e70d4986778c87c81b
Karan Ram <Karanram73005@gmail.com>
2026-02-10 20:14:49 +0530

    added file

---
+++ lower-to-uppercase.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char charLower;
+    scanf("%c", &charLower);
+    int charUpper = charLower - 32;
+    printf("The uppercase of %c is %c", charLower, charUpper);
+    return 0;
+}
